e72d66c1347585c9b299af312b05589e1479e624
Leothon Wong <langmancaizi@gmail.com>
2017-05-18 11:42:55 +0800

    first use kotlin language to program android

---
+++ app/src/main/java/com/example/a10483/kotlintest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.a10483.kotlintest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+//import kotlinx.android.synthetic.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
